da403c10cf17a8fc553ec863f5c097ed84efea8c
Tenzin Dawa <tnzngdw@gmail.com>
2023-02-05 21:55:23 +0530

    Add files via upload

---
+++ streaming-service/src/main/java/com/apps/reactive/streamingservice/StreamingService.java
@@ -0,0 +1,22 @@
+package com.apps.reactive.streamingservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class StreamingService {
+
+    private static final String VIDEO_FORMAT = "classpath:videos/%s.mp4";
+
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    public Mono<Resource> getVideo(String title) {
+        return Mono.fromSupplier(() -> this.resourceLoader.getResource(String.format(VIDEO_FORMAT, title)));
+    }
+}
+
